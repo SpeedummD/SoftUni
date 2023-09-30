fa73881e52f30bc971170ea4cd3fc6322acadc90
SpeedummD <146203148+SpeedummD@users.noreply.github.com>
2023-09-30 12:03:26 +0300

    02. Nums 1...10

---
+++ First Steps In Coding - Lab/Nums1To10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Nums1To10
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
