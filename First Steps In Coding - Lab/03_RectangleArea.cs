a483750622ae9313ed385081994e52269b4710d3
SpeedummD <146203148+SpeedummD@users.noreply.github.com>
2023-10-01 15:20:40 +0300

    03. Rectangle Area

---
+++ First Steps In Coding - Lab/03_RectangleArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class RectangleArea
+{
+    public static void Main(string[] args)
+    { 
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(a * b);
+    }
+}
